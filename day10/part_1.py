3737e09de4e2c5f608c666a0ac0d9a76d104ddff
Morten Hauge <Morten.hauge97@gmail.com>
2020-12-10 22:45:37 +0100

    part_1 is now significantly prettier

--- day10/part_1.py
+++ day10/part_1.py
@@ -4,56 +4,44 @@ with open("input.txt", "r") as f:
 jolt_ratings = [int(line.strip()) for line in data]
 jolt_ratings.append(0) # charging outlet
 
-jolt_lookup = {}
+jolts = {}
 
 for i, rating in enumerate(jolt_ratings):
-    jolt_lookup[rating] = [
+    jolts[rating] = [
         j for j in range(rating + 1, rating + 4) if j in jolt_ratings]
 
 _max = max(jolt_ratings)
-jolt_lookup[_max] = [_max + 3]
-
-for rating in sorted(jolt_lookup.items()):
-    print(rating)
-
-print("")
+jolts[_max] = [_max + 3] # final adapter
 
 while True:
     all_assigned = True
 
-    for key, rating in sorted(jolt_lookup.items()):
-        if len(rating) == 1:
-            for r in jolt_lookup:
-                if r != key:
-                    try:
-                        possible = None
-
-                        if len(jolt_lookup[r]) != 1:
-                            possible = jolt_lookup[r].remove(rating[0])
-                    except ValueError:
-                        pass
-
-                    if possible:
-                        jolt_lookup[r] = possible
-
-        else:
+    for key, rating in jolts.items():
+        if len(rating) != 1:
             all_assigned = False
+            continue
+
+        for r in jolts:
+            if r == key or len(jolts[r]) == 1:
+                continue
+            
+            if rating[0] not in jolts[r]:
+                continue
+
+            options = jolts[r].remove(rating[0])
+            jolts[r] = options if options else jolts[r]
 
     if all_assigned:
         break
     
-one_diffs = 0
-three_diffs = 0
+diffs = [0, 0]
 
-for key, value in jolt_lookup.items():
-    rating = value[0]
+for key, values in jolts.items():
+    difference = values[0] - key
 
-    if rating - key == 1:
-        one_diffs += 1
-    elif rating - key == 3:
-        three_diffs += 1
+    if difference == 1:
+        diffs[0] += 1
+    elif difference == 3:
+        diffs[1] += 1
 
-print(one_diffs)
-print(three_diffs)
-
-print(one_diffs * three_diffs)
+print(diffs[0] * diffs[1])
