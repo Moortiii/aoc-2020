5d275cba551407b5bbd780d05ccc5158c0473213
= <=>
2020-12-20 16:59:34 +0100

    Day 12 - Part 2 uploaded

--- day12/part_2.py
+++ day12/part_2.py
@@ -1,3 +1,6 @@
+import numpy as np
+import math
+
 with open("input.txt", "r") as f:
     data = [l.strip() for l in f.readlines()]
 
@@ -10,10 +13,9 @@ for entry in data:
     instructions.append((direction, amount))
 
 waypoint = (10, 1) # east, north
-ship_degrees = 90
-ship_direction = 'E'
 east = 0
 north = 0
+
 
 def move_waypoint(east, north, direction, amount):
     if direction == 'E':
@@ -27,32 +29,41 @@ def move_waypoint(east, north, direction, amount):
 
     return east, north
 
-def move(east, north, waypoint, amount):
-    east += waypoint[0] * amount
+def move_ship(east, north, waypoint, amount):
+    east  += waypoint[0] * amount
     north += waypoint[1] * amount
 
     return east, north
 
-def turn(waypoint, direction, ship_direction, ship_degrees, amount):
-    # Calculate new path of travel
+def rotate_origin_only(waypoint, direction, radians):
+    """Only rotate a point around the origin (0, 0)."""
+    x, y = waypoint
+
+    radians = np.radians(radians)
+
     if direction == 'L':
-        ship_degrees = (ship_degrees - amount) % 360
-    elif direction == 'R':
-        ship_degrees = (ship_degrees + amount) % 360
+        radians = -radians
 
-    # Convert degrees to direction
-    new_direction = None
+    xx = x * math.cos(radians) + y * math.sin(radians)
+    yy = -x * math.sin(radians) + y * math.cos(radians)
 
-    if ship_degrees == 90:
-        new_direction = 'E'
-    elif ship_degrees == 180:
-        new_direction = 'S'
-    elif ship_degrees == 270:
-        new_direction = 'W'
-    else:
-        new_direction = 'N'
+    return round(xx), round(yy)
+
+def rotate_waypoint(waypoint, direction, amount):
+    theta = np.radians(amount)
     
-    return new_direction, ship_degrees
+    print("Amount:", amount)
+    if direction == 'L':
+        theta = -theta
+    
+    r = np.array((
+        (np.cos(theta), -np.sin(theta)),
+        (np.sin(theta),  np.cos(theta))
+    ))
+
+    print("Rotation:", r.dot(waypoint))
+
+    return np.floor(r.dot(waypoint))
 
 def manhattan_distance(x, y):
     return abs(x) + abs(y)
@@ -66,24 +77,20 @@ for instruction in instructions:
         "CURRENT - ",
         "East:", east,
         "North:", north,
-        "Direction:", ship_direction,
-        "Degrees:", ship_degrees,
         "Waypoint", waypoint,
     )
 
-    if direction in 'NSEW': # adjust direction and travel
+    if direction in 'NSEW':
         waypoint = move_waypoint(waypoint[0], waypoint[1], direction, amount)
-    elif direction in 'LR': # turn the ship
-        ship_direction, ship_degrees = turn(waypoint, ship_degrees, amount)
-    elif direction == 'F': # travel in current direction
-        east, north = move(east, north, waypoint, amount)
+    elif direction in 'LR':
+        waypoint = rotate_origin_only(waypoint, direction, amount)
+    elif direction == 'F':
+        east, north = move_ship(east, north, waypoint, amount)
     
     print(
         "REPLACE - ",
         "East:", east,
         "North:", north,
-        "Direction:", ship_direction,
-        "Degrees:", ship_degrees,
         "Waypoint", waypoint,
         "\n"
     )
@@ -92,8 +99,6 @@ print(
     "FINAL - ",
     "East:", east,
     "North:", north,
-    "Direction:", ship_direction,
-    "Degrees:", ship_degrees,
     "Waypoint", waypoint,
     "\n"
 )
